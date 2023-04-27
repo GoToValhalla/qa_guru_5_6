65013ebebbcc9980476e95002b5edda93d112a60
Russkikh Konstanin <russ.kostua83@gmail.com>
2023-04-27 19:19:50 +0300

    add README

---
+++ collections_examples.py
@@ -0,0 +1,18 @@
+from collections import defaultdict
+
+d = defaultdict(int)
+
+print(d)
+
+d = defaultdict(list)
+
+d["users"].append("Marina")
+
+print(d)
+
+from collections import namedtuple
+
+user = namedtuple("user", ["name", "age"])
+user1 = user(name="Marina", age= 23)
+
+print(user1)
