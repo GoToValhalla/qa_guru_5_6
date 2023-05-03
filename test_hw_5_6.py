3e5086e5ca447ae4b02588cde500b7fb17a94b5f
Russkikh Konstanin <russ.kostua83@gmail.com>
2023-05-03 17:22:17 +0300

    add test_readable_function

--- test_hw_5_6.py
+++ test_hw_5_6.py
@@ -76,19 +76,26 @@ def test_find_suitable_user():
 # >>> open_browser(browser_name="Chrome")
 # "Open Browser [Chrome]"
 
+
+
+
 def test_readable_function():
     open_browser(browser_name="Chrome")
     go_to_companyname_homepage(page_url="https://companyname.com")
     find_registration_button_on_login_page(page_url="https://companyname.com/login", button_text="Register")
 
 def open_browser(browser_name):
-    actual_result = None
+    actual_result = open_browser.__name__.replace("_", " ").title() + f" [{browser_name}]"
+    print(end='\n')
+    print(actual_result, end='\n')
     assert actual_result == "Open Browser [Chrome]"
 
 def go_to_companyname_homepage(page_url):
-    actual_result = None
+    actual_result = go_to_companyname_homepage.__name__.replace("_", " ").title() + f" [{page_url}]"
+    print(actual_result, end='\n')
     assert actual_result == "Go To Companyname Homepage [https://companyname.com]"
 
 def find_registration_button_on_login_page(page_url, button_text):
-    actual_result = None
+    actual_result = find_registration_button_on_login_page.__name__.replace("_", " ").title() + f" [{page_url}, {button_text}]"
+    print(actual_result, end='\n')
     assert actual_result == "Find Registration Button On Login Page [https://companyname.com/login, Register]"
